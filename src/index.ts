019759b88c7122efd5d4839e7f9f573c7406d38f
woohm402 <woohm404@gmail.com>
2023-07-28 22:14:52 +0900

    init server

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use("/health-check", (req, res) => {
+  return res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  `Server is running on port ${port}`;
+});
