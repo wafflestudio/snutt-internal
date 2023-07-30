24096f3d773b34a7b0dc875fd2978e792b44604c
woohm402 <woohm404@gmail.com>
2023-07-31 01:24:24 +0900

    due 지난 거 확인 기능 구현

--- src/infrastructures/createKanbanService.ts
+++ src/infrastructures/createKanbanService.ts
@@ -1,6 +1,12 @@
 import { MessengerClient } from '../clients/messengerClient';
+import { Card } from '../entities/kanban';
+import { Member } from '../entities/member';
 import { KanbanRepository } from '../repositories/kanbanRepository';
 import { KanbanService } from '../services/kanbanService';
+
+enum KanbanAbnormalReason {
+  DUE_DATE_PASSED = 'DUE_DATE_PASSED',
+}
 
 export const createKanbanService = ({
   repositories: [kanbanRepository],
@@ -9,11 +15,53 @@ export const createKanbanService = ({
   repositories: [KanbanRepository];
   clients: [MessengerClient];
 }): KanbanService => {
+  const checkKanbanAbnormal = (card: Card): { abnormal: false } | { abnormal: true; reason: KanbanAbnormalReason } => {
+    if (card.status !== 'Done' && card.status !== 'Archived' && card.status !== 'Completed' && card.due !== null) {
+      const today = new Date().getTime();
+      const dueDate = Array.isArray(card.due) ? card.due[1].getTime() : card.due.getTime();
+      if (today > dueDate) return { abnormal: true, reason: KanbanAbnormalReason.DUE_DATE_PASSED };
+    }
+
+    return { abnormal: false };
+  };
+
   return {
     sendAbnormalCardStatuses: async () => {
       const cards = await kanbanRepository.listCards();
-      console.log(cards);
-      await messengerClient.sendThread('hello', ['hi', 'bye']);
+      const abnormalCards = cards.reduce<(Card & { reason: KanbanAbnormalReason })[]>((acc, card) => {
+        const result = checkKanbanAbnormal(card);
+        if (!result.abnormal) return acc;
+        return [...acc, { ...card, reason: result.reason }];
+      }, []);
+
+      await messengerClient.sendThread(
+        '칸반 이슈',
+        abnormalCards.map((card) => {
+          const assignees = card.assignee.map((a) => `<@${MEMBER_SLACK_ID_MAP[a]}>`).join(' ');
+          const title = `<${card.url}|${card.title}>`;
+          const reason = REASON_MESSAGE_MAP[card.reason];
+          return `${assignees} ${title}\n\n${reason}`;
+        }),
+      );
     },
   };
+};
+
+const REASON_MESSAGE_MAP: Record<KanbanAbnormalReason, string> = {
+  [KanbanAbnormalReason.DUE_DATE_PASSED]: '설정된 due date 가 지났습니다. due를 변경하거나 상태를 업데이트해 주세요.',
+};
+
+// TODO: 적절한 곳으로 옮기기. 근데 템플릿이 여깄어서 되려나..
+const MEMBER_SLACK_ID_MAP: Record<Member, string> = {
+  [Member.WOOHM402]: 'U01JQM3GNBW',
+  [Member.SHP7724]: 'U030UCYBZC3',
+  [Member.JUTAK97]: 'U030UCYA7U3',
+  [Member.DAVIN111]: 'ULHAW7P7Z',
+  [Member.HANK_CHOI]: 'URJJGG33J',
+  [Member.ARS_KI_00]: 'URW5RHG1L',
+  [Member.JHVICTOR4]: 'US9MACQUV',
+  [Member.EASTSHINE2741]: 'U04EC1QEP6V',
+  [Member.PENG_U_0807]: 'U03171C4MFT',
+  [Member.CHAEMIN2001]: 'U030WM38PM2',
+  [Member.EUXXNIA]: 'U04F0NCC9L4',
 };
--- src/infrastructures/createNotionKanbanClient.ts
+++ src/infrastructures/createNotionKanbanClient.ts
@@ -1,8 +1,9 @@
 import { KanbanClient } from '../clients/kanbanClient';
 import { Client } from '@notionhq/client';
 import { Card } from '../entities/kanban';
+import { Member } from '../entities/member';
 
-export const createNotioKanbanClient = ({
+export const createNotionKanbanClient = ({
   notionToken,
   databaseId,
 }: {
@@ -13,8 +14,46 @@ export const createNotioKanbanClient = ({
 
   return {
     listCards: async () => {
-      const cards = await notion.databases.query({ database_id: databaseId });
-      return cards.results as unknown as Card[];
+      const { results } = (await notion.databases.query({ database_id: databaseId })) as unknown as {
+        results: {
+          id: string;
+          url: string;
+          properties: {
+            Status: { status: { name: Card['status'] } };
+            'Due Date': { date: { start: string; end: string | null } | null };
+            Assignee: { people: { id: string; name: string }[] };
+            Name: { title: { plain_text: string }[] };
+          };
+        }[];
+      };
+
+      return results.map((c) => ({
+        id: c.id,
+        url: c.url,
+        assignee: c.properties.Assignee.people.map((p) => MEMBER_NOTION_ID_MAP[p.id]).filter((m): m is Member => !!m),
+        status: c.properties.Status.status.name,
+        title: c.properties.Name.title.map((t) => t.plain_text).join(''),
+        due:
+          c.properties['Due Date'].date === null
+            ? null
+            : c.properties['Due Date'].date.end
+            ? [new Date(c.properties['Due Date'].date.start), new Date(c.properties['Due Date'].date.end)]
+            : new Date(c.properties['Due Date'].date.start),
+      }));
     },
   };
+};
+
+const MEMBER_NOTION_ID_MAP: Record<string, Member | undefined> = {
+  'a60a2b22-e58c-4cf8-a100-764f60cac65c': Member.WOOHM402,
+  '12233745-30b8-49ac-84aa-d324f7041c23': Member.SHP7724,
+  '2a3b479e-13cb-40a7-9aaf-4be38111e97a': Member.JUTAK97,
+  '5d1120a3-0a70-4a30-8845-ec17f697f34f': Member.DAVIN111,
+  'e11c16b0-537b-4c61-a53a-e707aa481896': Member.HANK_CHOI,
+  '4b9f3337-bf6e-4385-bcf2-633b6c0a12b4': Member.ARS_KI_00,
+  '8d57adea-038f-436a-a098-cd53ba5781ae': Member.JHVICTOR4,
+  '84254454-3dc7-44e1-808f-7fe2b363a217': Member.EASTSHINE2741,
+  'a8186318-c9d9-42d7-9b2b-303f15ab8367': Member.PENG_U_0807,
+  '5f018d07-d2d8-4845-b4f1-e7dbdb49015e': Member.CHAEMIN2001,
+  'e3c4232e-41ce-4189-90f4-121c7cda69f8': Member.EUXXNIA,
 };
