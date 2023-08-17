63785dfaa51afb45a39d7b5cd3d66536646f237b
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-08-18 00:48:25 +0900

    🧑‍💻 칸반 체크 메세지 자동화 (테스트용 - 매일 오전 12시 50분으로 설정됨) (#23)

--- apps/kanban-check-script/src/index.ts
+++ apps/kanban-check-script/src/index.ts
@@ -5,25 +5,27 @@ import { createKanbanService } from './infrastructures/createKanbanService';
 import { createNotionKanbanClient } from './infrastructures/createNotionKanbanClient';
 import { createSlackMessengerClient } from './infrastructures/createSlackMessengerClient';
 
-dotenv.config({ path: './.env.local' });
+dotenv.config({ path: './.env.local' }); // for local development
 
 const NOTION_KANBAN_DATABASE_ID = process.env.NOTION_KANBAN_DATABASE_ID;
-const NOTION_TOKEN = process.env.NOTION_TOKEN;
-const SLACK_TOKEN = process.env.SLACK_TOKEN;
+const NOTION_KANBANBOT_TOKEN = process.env.NOTION_KANBANBOT_TOKEN;
+const SLACK_TTUNS_TOKEN = process.env.SLACK_TTUNS_TOKEN;
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
 
 if (!NOTION_KANBAN_DATABASE_ID) throw new Error();
-if (!NOTION_TOKEN) throw new Error();
-if (!SLACK_TOKEN) throw new Error();
+if (!NOTION_KANBANBOT_TOKEN) throw new Error();
+if (!SLACK_TTUNS_TOKEN) throw new Error();
 if (!SLACK_CHANNEL) throw new Error();
 
 const kanbanService = createKanbanService({
   repositories: [
     createKanbanRepository({
-      clients: [createNotionKanbanClient({ databaseId: NOTION_KANBAN_DATABASE_ID, notionToken: NOTION_TOKEN })],
+      clients: [
+        createNotionKanbanClient({ databaseId: NOTION_KANBAN_DATABASE_ID, notionToken: NOTION_KANBANBOT_TOKEN }),
+      ],
     }),
   ],
-  clients: [createSlackMessengerClient({ slackToken: SLACK_TOKEN, slackChannel: SLACK_CHANNEL })],
+  clients: [createSlackMessengerClient({ slackToken: SLACK_TTUNS_TOKEN, slackChannel: SLACK_CHANNEL })],
 });
 
 kanbanService.sendAbnormalCardStatuses();
