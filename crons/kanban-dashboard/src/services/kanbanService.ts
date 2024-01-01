093f3cae302db1da152848b82a37087f1e45064c
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-01-01 21:12:23 +0900

    ♻️ kanban-dashboard, kanban-issue-checker 하나로 합친다 (#59)

--- crons/kanban-dashboard/src/services/kanbanService.ts
+++
@@ -1,3 +0,0 @@
-export type KanbanService = {
-  sendDashboard: () => Promise<void>;
-};
--- crons/kanban-reporter/src/infrastructures/createKanbanService.ts
+++ crons/kanban-reporter/src/infrastructures/createKanbanService.ts
@@ -12,6 +12,46 @@ export const createKanbanService = ({
   messengerPresenter: MessengerPresenter;
 }): KanbanService => {
   return {
+    sendDashboard: async () => {
+      const cards = await kanbanRepository.listCards({
+        status: {
+          'To Do': false,
+          'In Progress': true,
+          'In Review': false,
+          Archived: false,
+          Backlog: false,
+          Released: false,
+          Done: false,
+        },
+      });
+
+      await messengerPresenter.sendThread(
+        ({ formatLink, formatEmoji, formatBold }) =>
+          [
+            `${formatBold('SNUTT 무슨 일이 일어나고 있나요?')} ${formatEmoji('wip')}`,
+            '',
+            ...cards.map(
+              (c) =>
+                `${formatEmoji(
+                  c.part
+                    ? (
+                        {
+                          [Part.ALL]: 'snutt',
+                          [Part.ANDROID]: 'android',
+                          [Part.DESIGN]: 'design',
+                          [Part.FRONTEND]: 'react',
+                          [Part.IOS]: 'ios',
+                          [Part.SERVER]: 'spring',
+                        } as const
+                      )[c.part]
+                    : ('null' as const),
+                )} ${formatLink(c.title, { url: c.url })}`,
+            ),
+          ].join('\n'),
+        [],
+      );
+    },
+
     sendAbnormalCardStatuses: async () => {
       const cards = await kanbanRepository.listCards({
         status: {
