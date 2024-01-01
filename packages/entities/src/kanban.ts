f8d35fd57b48e7293903acfc82b135d769012367
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-01-01 22:35:14 +0900

    ✨ start 가 null 인 경우를 배제한다 (#61)

--- packages/entities/src/kanban.ts
+++ packages/entities/src/kanban.ts
@@ -7,5 +7,5 @@ export type Card = {
   assignee: Member[];
   status: 'Backlog' | 'To Do' | 'In Progress' | 'In Review' | 'Archived' | 'Done' | 'Released';
   title: string;
-  schedule: [Date | null, Date] | null;
+  schedule: [Date, Date] | null;
 };
